44e11f9eb9d31fdec3f4032d3d025b11c0dcb994
Art Elvin <cs50artelvin@gmail.com>
2025-01-24 17:20:05 +0800

    helloworld test

--- week/0/helloworld.c
+++ week/0/helloworld.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
-    printf("Hello world!\n");
+    string name = get_string("What's your name? ");
+
+    printf("Hello, %s!\n", name);
 }
