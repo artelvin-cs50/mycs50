d1108515e53a62cf66afd23086accbda9f63ad37
Art Elvin <cs50artelvin@gmail.com>
2025-01-24 14:59:21 +0000

    Fri, Jan 24, 2025, 2:59 PM +00:00

--- week/1/credit/credit.c
+++ week/1/credit/credit.c
@@ -3,7 +3,15 @@
 #include <math.h>
 
 
+// American Express uses 15-digit numbers | American Express numbers start with 34 or 37
+// MasterCard uses 16-digit numbers | MasterCard numbers start with 51, 52, 53, 54, or 55
+// Visa uses 13- and 16-digit numbers | Visa numbers start with 4
+
+
 int main(void)
 {
-    
+    // Prompt user for card number
+    long card_number = get_long("Number: ");
+
+
 }
