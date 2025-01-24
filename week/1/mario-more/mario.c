5b4f5970324329ebe98a778f9892b97473cb2c9f
Art Elvin <cs50artelvin@gmail.com>
2025-01-24 17:57:29 +0800

    mario-more

---
+++ week/1/mario-more/mario.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Prompt for user input and handle invalid ones
+    int height = 0;
+    do {
+        height = get_int("Height: ");
+    } while(height < 1);
+
+
+    // Build pyramid
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = height - 1; j > i; j--) // Print `(height - 1) - i` blankspaces
+        {
+            printf(" ");
+        }
+
+        for(int k = 0; k <= i; k++) // Print `i + 1` hashes (left side of pyramid)
+        {
+            printf("#");
+        }
+
+        printf("  "); // Add center gap
+
+        for(int u = 0; u <= i; u++) // Print `i + 1` hashes (right side of pyramid)
+        {
+            printf("#");
+        }
+
+        printf("\n"); // Go to next row
+    }
+}
