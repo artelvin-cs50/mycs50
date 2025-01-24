a13f52e68031d456534980a8c780ad74b3047b4f
Art Elvin <cs50artelvin@gmail.com>
2025-01-24 15:33:44 +0000

    Fri, Jan 24, 2025, 3:33 PM +00:00

--- week/1/credit/credit.c
+++ week/1/credit/credit.c
@@ -8,9 +8,9 @@
 // Visa uses 13- and 16-digit numbers | Visa numbers start with 4
 
 
-int get_length(long number);    // Function to get card length
-int get_initial_digits(long number, int length)    // Function to get card's initial digits
-string get_card_type(int length, int digits)    // Function to identify card type using length and initial digits
+// int get_length(long number);    // Function to get card length
+// int get_initial_digits(long number, int length)    // Function to get card's initial digits
+string get_card_type(long number);    // Function to identify card type using length and initial digits
 
 
 int main(void)
@@ -18,41 +18,57 @@ int main(void)
     // Prompt user for card number
     long cardNumber = get_long("Number: ");
 
-    string cardType = get_card_type(get_length(cardNumber), get_initial_digits())
+    string cardType = get_card_type(cardNumber);
+
+    printf("%s\n", cardType);
 
 }
 
 
-int get_length(long number)
+// int get_length(long number)
+// {
+//     int count = 0;
+//     while(number > 0)
+//     {
+//         number /= 10;
+//         count++;
+//     }
+//     return count;
+// }
+
+
+// int get_initial_digits(long number, int length)
+// {
+//     return number / (pow(length - 2));
+// }
+
+
+string get_card_type(long number)
 {
-    int count = 0;
+    int length = 0;
     while(number > 0)
     {
         number /= 10;
-        count++;
+        length++;
     }
-    return count;
-}
 
+    printf("%d\n", length);
 
-int get_initial_digits(long number, int length)
-{
-    return number / (pow(length - 2));
-}
+    int digits = number / (pow(10, length - 2));
+    printf("%d\n", digits);
 
-
-int get_card_type(int length, int digits)
-{
     if(length == 15 && (digits == 34 || digits == 37))
     {
         return "AMEX";
     }
-    else if(length == 16 && (digits >= 51 || digits <= 55))
+    else if(length == 16 && (digits >= 51 && digits <= 55))
     {
         return "MASTERCARD";
     }
-    else if((length == 13 || length == 16) && (digits >= 40 || digits <= 49))
+    else if((length == 13 || length == 16) && (digits >= 40 && digits <= 49))
     {
         return "VISA";
     }
+    else
+        return "INVALID";
 }
