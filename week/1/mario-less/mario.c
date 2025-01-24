a185f33ad6bd80503f8d6fc6765339b267881520
Art Elvin <cs50artelvin@gmail.com>
2025-01-24 17:49:17 +0800

    mario-less

---
+++ week/1/mario-less/mario.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Prompt user for height and handle invalid ones
+    int height = 0;
+    do {
+        height = get_int("Height: ");
+    } while(height < 1);
+
+
+    // Build pyramid
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = height - 1; j > i; j--) // Print `(height - 1) - i` blankspaces
+        {
+            printf(" ");
+        }
+
+        for(int k = 0; k <= i; k++) // Print `i + 1` hashes
+        {
+            printf("#");
+        }
+
+        printf("\n"); // Go to next row
+    }
+}
