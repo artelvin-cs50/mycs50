bedda1c38bde7309a8dfb5d3d0881da61257b531
Art Elvin <cs50artelvin@gmail.com>
2025-01-24 15:04:18 +0000

    Fri, Jan 24, 2025, 3:04 PM +00:00

--- week/1/credit/credit.c
+++ week/1/credit/credit.c
@@ -8,6 +8,10 @@
 // Visa uses 13- and 16-digit numbers | Visa numbers start with 4
 
 
+int get_length(long number);    // Function to get card length
+int get_initial_digits(long number, int length)    // Function to get card's first two digits (or only first for VISA)
+
+
 int main(void)
 {
     // Prompt user for card number
